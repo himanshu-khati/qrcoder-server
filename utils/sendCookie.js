5eda668bb385ccf7888c8d1fb65db8ad74530da8
Himanshu Khati <146989963+himanshu-khati@users.noreply.github.com>
2025-02-12 16:12:46 +0530

    Update sendCookie.js

--- utils/sendCookie.js
+++ utils/sendCookie.js
@@ -1,17 +1,12 @@
 import jwt from "jsonwebtoken";
-
 export const sendCookie = (user, res, message, statusCode = 200) => {
-  const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET, {
-    expiresIn: "12h",
-  });
-
+  const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET);
   res.cookie("token", token, {
     httpOnly: true,
-    maxAge: 12 * 60 * 60 * 1000, // 12 hours
-    sameSite: "None", // Allow cross-origin requests
-    secure: process.env.NODE_ENV !== "Development", // Secure in production
+    maxAge: 12 * 60 * 60 * 1000,
+    sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "Development" ? false : true,
   });
-
   res.status(statusCode).json({
     success: true,
     message,

546ab1e7b8d7d04063d4429d1c918f325765d3df
Himanshu Khati <146989963+himanshu-khati@users.noreply.github.com>
2025-02-12 16:15:39 +0530

    Update Auth.js

--- controller/Auth.js
+++ controller/Auth.js
@@ -118,19 +118,21 @@ export const login = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
   try {
-    res.clearCookie("token", {
-      expires: new Date(0),
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "Production", 
-      sameSite: "None", 
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "User logged out successfully",
-    });
+    res
+      .status(200)
+      .clearCookie("token", "", {
+        expires: new Date(0),
+        sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "Development" ? false : true,
+      })
+      .json({
+        success: true,
+        message: `User logged out successfully`,
+      });
   } catch (error) {
-    return next(new ErrorHandler(`Error while logging out: ${error.message}`, 501));
+    return next(
+      new ErrorHandler(`Errror while logging out: ${error.message}`, 501)
+    );
   }
 };
 

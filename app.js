96c21e22183f250a2e71ac79951d54628abe9232
Himanshu Khati <146989963+himanshu-khati@users.noreply.github.com>
2025-02-12 15:39:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
